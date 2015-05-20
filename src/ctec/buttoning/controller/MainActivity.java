8da1dd7cd05ea689e4cb14871b23f3848d227f8c
1upsoda <1upsoda@gmail.com>
2015-05-20 07:53:06 -0600

    recommiting all

--- src/ctec/buttoning/controller/MainActivity.java
+++ src/ctec/buttoning/controller/MainActivity.java
@@ -1,38 +1,40 @@
 package ctec.buttoning.controller;
 
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity 
 {
 
 	private Button firstScreenButton;
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+
+	/**
+	 * Override
+	 * 
+	 * @param savedInstanceState
+	 */
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		firstScreenButton = (Button) findViewById(R.id.firstScreenButton);
-		
+		setContentView(R.layout.activity_list_item);
+
+		firstScreenButton = (Button) findViewById(R.id.button1);
+
 		setupListeners();
 	}
-	
-	private void setupListeners()
-	{
-		firstScreenButton.setOnClickListener(new View.OnClickListener()
+
+	private void setupListeners() {
+		firstScreenButton.setOnClickListener(new View.OnClickListener() 
 		{
-			
+
 			@Override
-			public void onClick(View currentView)
-			{
+			public void onClick(View currentView) {
 				// TODO Auto-generated method stub
-				Intent otherScreenIntent = new Intent(currentView.getContext(), SecondScreenActivity.class);
+				Intent otherScreenIntent = new Intent(currentView.getContext(),
+						SecondScreenActivity.class);
 				startActivityForResult(otherScreenIntent, 0);
 			}
 		});
